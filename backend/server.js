402cf965f1006a2504ccdb95f9be4c39af674f71
soumyapriyadarshi99 <das.soumyapd@gmail.com>
2022-10-22 02:12:16 +0530

    signin last video left

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 import express from "express";
-import data from "./data.js";
+// import data from "./data.js";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import seedRouter from "./routes/seedRoutes.js";
 import productRouter from "./routes/productRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 const corsOptions = {
   origin: "*",
@@ -21,9 +22,16 @@ mongoose
 const app = express();
 
 app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/seed', seedRouter);
-app.use('/api/products', productRouter);
+app.use("/api/seed", seedRouter);
+app.use("/api/products", productRouter);
+app.use("/api/users", userRouter );
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 5000;
 
